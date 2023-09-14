2f4fd44ed33fa58baf14bfd9850169733040b607
sindhusunki789@gmail.com <sindhusunki789@gmail.com>
2023-09-14 15:49:29 +0530

    first commit

---
+++ Model/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace Assignment4.Model
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Assignment4.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Assignment4DbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Assignment4DbContext") ?? throw new InvalidOperationException("Connection string 'Assignment4DbContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
